6f37d726d7014bd7d5cdceb2a58700335e7e0e89
Nermen77 <137504991+NermenHamda77@users.noreply.github.com>
2024-05-18 14:38:43 +0300

    Initial Commit

---
+++ SportsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SportsApp
+//
+//  Created by Ner Meen on 18/05/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
