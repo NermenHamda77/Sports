5b088b791fb64775651e2e9eac97b90539df4718
Nermen77 <137504991+NermenHamda77@users.noreply.github.com>
2024-05-23 23:12:06 +0300

    add the ui of favorite screen

---
+++ SportsApp/modules/favourites/FavTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  FavTableViewCell.swift
+//  SportsApp
+//
+//  Created by Ner Meen on 23/05/2024.
+//
+
+import UIKit
+
+class FavTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var favLeagueName: UILabel!
+    @IBOutlet weak var favLeagueImage: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ SportsApp/modules/favourites/FavoriteViewController.swift
@@ -0,0 +1,53 @@
+//
+//  FavoriteViewController.swift
+//  SportsApp
+//
+//  Created by Ner Meen on 23/05/2024.
+//
+
+import UIKit
+
+class FavoriteViewController: UIViewController , UITableViewDelegate, UITableViewDataSource{
+    
+
+    @IBOutlet weak var tableView: UITableView!
+    
+  
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.register(UINib(nibName: "FavTableViewCell", bundle: nil), forCellReuseIdentifier: "favCell")
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "favCell", for: indexPath) as! FavTableViewCell
+        
+        cell.favLeagueImage.layer.cornerRadius = 40.0
+        cell.favLeagueImage.layer.masksToBounds = true
+        return cell
+    }
+    
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+        
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
