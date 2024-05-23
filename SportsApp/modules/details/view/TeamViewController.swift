4d685a9f19514d1a8a63e326e8fc7e55d280e7bf
Nermen77 <137504991+NermenHamda77@users.noreply.github.com>
2024-05-23 23:21:16 +0300

    add some changes in ui

--- SportsApp/modules/details/view/TeamViewController.swift
+++ SportsApp/modules/details/view/TeamViewController.swift
@@ -13,9 +13,12 @@ class TeamViewController: UIViewController, UITableViewDelegate, UITableViewData
     @IBOutlet weak var teamName: UILabel!
     @IBOutlet weak var teamImage: UIImageView!
     @IBOutlet weak var sportImage: UIImageView!
-    
+  
     override func viewDidLoad() {
         super.viewDidLoad()
+        // to reduce opacity
+        sportImage.alpha = 0.9
+
 
         let nib = UINib(nibName: "PlayersTableViewCell", bundle: nil)
                tableView.register(nib, forCellReuseIdentifier: "playersCell")
@@ -48,3 +51,33 @@ class TeamViewController: UIViewController, UITableViewDelegate, UITableViewData
     */
 
 }
+
+/*
+ //
+ //  HomeCollectionViewCell.swift
+ //  SportsApp
+ //
+ //  Created by Ner Meen on 20/05/2024.
+ //
+
+ import UIKit
+
+ class HomeCollectionViewCell: UICollectionViewCell {
+     @IBOutlet weak var sportImage: UIImageView!
+     @IBOutlet weak var sportName: UILabel!
+     
+     // Add an overlay view programmatically if not done in the xib
+     var overlayView: UIView!
+     
+     override func awakeFromNib() {
+         super.awakeFromNib()
+         
+         // Initialize the overlay view
+         overlayView = UIView(frame: sportImage.bounds)
+         overlayView.backgroundColor = UIColor.black.withAlphaComponent(0.5) // Adjust alpha as needed
+         overlayView.autoresizingMask = [.flexibleWidth, .flexibleHeight] // To resize with image view
+         sportImage.addSubview(overlayView)
+     }
+ }
+
+ */
